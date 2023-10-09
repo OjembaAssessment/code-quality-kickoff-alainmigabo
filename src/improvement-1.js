7c45d69964a050d0f406f5e50ed266705afb5b3e
alainmigabo <alainmigabo2k03@gmail.com>
2023-10-09 15:20:49 +0200

    Improvement 1

--- src/improvement-1.js
+++ src/improvement-1.js
@@ -5,12 +5,9 @@
  */
 
 // FUNCTION 1
-export default function cntRbt(ls) {
-  var cnt = 0;
-  for (var i = 0; i < ls.length; i++) {
-    if (ls[i] == "R") {
-      cnt++;
-    }
-  }
-  return cnt;
+export default function countRbt(ls) {
+  const arrayOfCharacters = ls.split('')
+  const arrayOfRs = arrayOfCharacters.filter(character => character == 'R')
+  const numberOfRs = arrayOfRs.length
+  return numberOfRs
 }
